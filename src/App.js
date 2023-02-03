36aa3b7d721d298d8cc6d39f40f1163d223366ea
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2023-02-02 21:35:34 -0800

    working on nav bar and setting up firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from "./routes/home/home.component";
+import SignIn from './routes/sign-in/sign-in.component';
 
 
 
@@ -12,9 +13,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element ={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
