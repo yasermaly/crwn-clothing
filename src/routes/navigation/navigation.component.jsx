a42c8e8b0b40883c3029ab2cc35e65ba4da94c27
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2023-02-24 16:55:02 -0800

    creating cart component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,11 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { UserContext } from "../../contexts/user.context";
 
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 import './navigation.styles.scss';
@@ -32,8 +34,9 @@ const Navigation = () => {
                 SIGN IN
               </Link>
             )}
-         
+         <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
