4eea32ae60b43180bc7d5efbfb25cc591abd5179
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2023-02-28 21:42:43 -0800

    styled components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 import { 
   signInWithGooglePopup, 
-  createUserDocumentFromAuth,
-  SignInAuthUserWithEmailAndPassword
+  SignInAuthUserWithEmailAndPassword,
  } from '../../utils/firebase/firebase.utils';
 
 import './sign-in-form.styles.scss'
@@ -34,19 +33,10 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-          const {user} = await SignInAuthUserWithEmailAndPassword(email, password);
+          await SignInAuthUserWithEmailAndPassword(email, password);
           resetFormFields();
         } catch (error) {
-          switch (error.code) {
-            case 'auth/wrong-password':
-              alert('incorrect password for email');
-              break
-            case 'auth/user-not-found':
-              alert('no user associated with this email');
-              break;
-            default:
-              console.log(error);
-          }
+          console.log('user sign in failed', error);
           }
         }
 
@@ -79,10 +69,14 @@ const SignInForm = () => {
             name="password"
             value={password}
           />
-          <div className='buttons-container'>
+          <div className="buttons-container">
             <Button type="submit">Sign In</Button>
-            <Button type='button' buttonType="google" onClick={SignInWithGoogle}>
-              Google Sign In
+            <Button
+              buttonType={BUTTON_TYPE_CLASSES.google}
+              type="button"
+              onClick={SignInWithGoogle}
+            >
+              Sign In With Google
             </Button>
           </div>
         </form>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,16 @@
 import { Fragment, useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from '../../contexts/cart.context';
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
-import './navigation.styles.scss';
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles';
 
 
 const Navigation = () => {
@@ -17,28 +19,24 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrwnLogo className="logo" />
-        </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
-            SHOP
-          </Link>
-          {
-            currentUser ? (
-              <span className="nav-link" onClick={signOutUser}>
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
+
+          {currentUser ? (
+              <NavLink as='span' onClick={signOutUser}>
                SIGN OUT
-              </span>
+              </NavLink>
             ) : ( 
-              <Link className="nav-link" to="/auth">
-                SIGN IN
-              </Link>
+              <NavLink to="/auth">SIGN IN</NavLink>
             )}
          <CartIcon />
-        </div>
+        </NavLinks>
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
