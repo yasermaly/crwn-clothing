f283af2442928fe4dd8cd0b7bfba5cde42ad6464
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2023-03-12 11:32:46 -0700

    routes

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from '../category/category.component';
 
-import CategoriesPreview from '../categories-preview/categories-preview.component';
 
 
 
