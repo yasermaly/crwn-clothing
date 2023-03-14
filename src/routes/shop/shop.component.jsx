7ee2ba65e404d44f85dbfdc7f7a93a63e3e7ca8c
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2023-03-14 16:53:48 -0700

    migrated category context to redux

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,32 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from '../category/category.component';
-
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategoriesMap } from '../../store/categories/category.action';
 
 
 
 const Shop = () => {
+  const dispatch = useDispatch();
 
-    return (
-      <Routes>
-        <Route index element={<CategoriesPreview />} />
-        <Route path=":category" element={<Category />} />
-      </Routes>
-    );
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments("categories");
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
+  );
 };
 
 export default Shop;
