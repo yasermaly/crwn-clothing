64de1ef3fed6aeeab6c670c611dfe2315d1a46f0
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2023-03-14 10:30:46 -0700

    converting to redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -42,15 +42,6 @@ export const UserProvider = ({ children }) => {
 
     const value = {currentUser, setCurrentUser};
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            if (user) {
-                createUserDocumentFromAuth(user);
-            }
-            setCurrentUser(user);
-        })
-        return unsubscribe
-    }, []);
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+import { store } from './store/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
